badfb935c6a6c02f45fa423d7770d2314af0c15b
Bruno Carneiro <tr301605@trf1.jus.br>
2023-09-08 13:24:04 -0300

    ajustando layout das tabelas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsultaTabelaComponent } from './cjf/consulta-tabela/consulta-tabela.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ConsultaTabelaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
